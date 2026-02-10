c5ad14f0daed7a57aa7cfc5a2c70cac0149c31af
alvastern <alva.stern02@gmail.com>
2026-02-10 20:22:58 +0100

    Började skapa ljust/mörkt läge

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,8 @@
 import "../css/main.scss";
 
 // Ändra till mörkt eller ljust läge
-let switch = document.getElementById("theme-switch");
+let themeSwitch = document.getElementById("theme-switch");
 
+themeSwitch.addEventListener('click', () => {
+    document.body.classList.toggle("dark", themeSwitch.checked)
+});
