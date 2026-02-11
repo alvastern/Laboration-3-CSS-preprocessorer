2ab75507652e0a034b0125dd95390ed58b041657
alvastern <alva.stern02@gmail.com>
2026-02-11 20:20:33 +0100

    Berättade för vite att jag vill ha flera HTML-sidor

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-export default {
-    css: {
-        devSourcemap: true
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  css: {
+    devSourcemap: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        sass: resolve(__dirname, "src/html/sass.html")
+      }
     }
-}
+  }
+});
