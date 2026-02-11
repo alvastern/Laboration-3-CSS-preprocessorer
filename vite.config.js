92ef979f194f49b004e71313990a3ea031762c38
alvastern <alva.stern02@gmail.com>
2026-02-11 20:38:31 +0100

    Ändrade om strukturen för att få navigationen att fungera i dist-mappen

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  css: {
-    devSourcemap: true
-  },
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        sass: resolve(__dirname, "src/html/sass.html")
+        sass: resolve(__dirname, "sass.html")
       }
     }
   }
