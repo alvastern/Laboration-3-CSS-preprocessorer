c4eb36e40a3bc395c1e8f77743aedf8c1edd1315
alvastern <alva.stern02@gmail.com>
2026-02-11 08:57:18 +0100

    Uppdaterade mörkt/ljust läge med media queries

--- src/js/main.js
+++ src/js/main.js
@@ -8,14 +8,23 @@ let themeSwitch = document.getElementById("theme-toggle");
 
 if (savedTheme === "dark") {
     document.body.classList.add("dark");
+    document.body.classList.remove("light");
     themeSwitch.checked = true;
+}
+
+if (savedTheme === "light") {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+    themeSwitch.checked = false;
 }
 
 themeSwitch.addEventListener('change', () => {
     if (themeSwitch.checked) {
         document.body.classList.add("dark");
+        document.body.classList.remove("light");
         localStorage.setItem("theme", "dark");
     } else {
+        document.body.classList.add("light");
         document.body.classList.remove("dark");
         localStorage.setItem("theme", "light")
     }
